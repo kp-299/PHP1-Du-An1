e3a02297e16759d0cbac040a95ff546d6e8065bc
KhangDeve <toanhoc123789456@gmail.com>
2026-05-14 10:08:54 +0000

    Thêm cấu trúc dự án với các tệp CSS và cấu hình Tailwind

--- views/app.php
+++ views/app.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>Demo PHP MVC</title>
+    <link rel="stylesheet" href="">
   </head>
   <body>
     <?= @$content ?>
